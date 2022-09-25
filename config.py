015bcec6517a8d828c05627b25c7176dce2748a0
Artemggq <111157938+Artemggq@users.noreply.github.com>
2022-09-25 22:28:10 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+bot_token = "5517318683:AAFJtmXPwSqZdWxLq1Hf-iAjrdIjjBvvso8" # @botfather
+
+QIWI_ACCOUNT = '79194114295' #НОМЕР КИВИ КОШЕЛЬКА
+QIWI_TOKEN = '2537922601d57f4e60c21529f7d393cd' #qiwi.com/api
+
+admin_id = '764405588' # можно узнать в @eyegodsbot ЭТО ЦЫФРЫ. ИХ НАДО ТАКЖЕ ВСТАВИТЬ В ФАЙЛЕ data=> admin.json
+
+oneday = 10 # ЕСЛИ МЕНЯЕТЕ ТУТ ЗНАЧЕНИЯ, МЕНЯЙТЕ И В MAIN.PY 35 СТРОКА цена за день подписки
+threeday = 140 # три дня подписки
+sevenday = 320 # неделя подписки
